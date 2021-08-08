a717c0391a29503318705eefb4178c9e497a63fa
sockRunMain <88559890+sockRunMain8392@users.noreply.github.com>
2021-08-08 13:06:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var server = http.Server(app);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-    //res.sendFile(__dirname + '/ios.css');
-    //res.sendFile(__dirname + '/main.js');
+    res.sendFile(__dirname + '/ios.css');
+    res.sendFile(__dirname + '/main.js');
 });
 
 server.listen(PORT, function() {
